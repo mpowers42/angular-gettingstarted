a0453d1c4d1f2b534035a4d995e1a5ada1649887
Powers <morgan.powers@accenture.com>
2021-07-21 09:59:11 -0400

    Using a component as a directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { Component } from "@angular/core";
   selector: 'pm-root', 
   template: `
   <div><h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
-    </div>
-    `
+    <pm-products></pm-products>
+  </div>
+  `
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
