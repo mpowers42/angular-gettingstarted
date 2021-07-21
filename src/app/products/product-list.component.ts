593222bd6eb491692cad342faf958519203c9606
Powers <morgan.powers@accenture.com>
2021-07-21 13:04:45 -0400

    Encapsulating component styles

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,11 @@
+import { listLazyRoutes } from "@angular/compiler/src/aot/lazy_routes";
 import { Component } from "@angular/core";
 import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent {
