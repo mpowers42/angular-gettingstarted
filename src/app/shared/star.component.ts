d7dc153ceb58d8d2c9d337f7e2e81979b5fda394
Powers <morgan.powers@accenture.com>
2021-07-22 11:42:09 -0400

    Passing data from a component using @output

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 
 @Component({
   selector: 'pm-star',
@@ -6,14 +6,16 @@ import { Component, Input, OnChanges } from "@angular/core";
   styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-  @Input() rating: number = 0;
-  cropWidth: number = 75;
+  @Input() rating = 0;
+  cropWidth = 75;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.cropWidth = this.rating * 75 / 5;
   }
 
   onClick(): void {
-    console.log('The rating ${this.rating} was clicked');
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 }
