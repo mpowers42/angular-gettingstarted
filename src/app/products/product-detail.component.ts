03c169d5e67fcfc8495780b6d18cbce4157c6096
Powers <morgan.powers@accenture.com>
2021-07-23 16:16:11 -0400

    Activating a route with code

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -10,11 +10,16 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 }
